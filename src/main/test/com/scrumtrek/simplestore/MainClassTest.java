ddb5f1f1fecf62c4566b543b1c1b4f2b2a98bd3f
kreig133 <kreig133@gmail.com>
2015-10-18 16:45:08 +0300

    new tests

---
+++ src/main/test/com/scrumtrek/simplestore/MainClassTest.java
@@ -0,0 +1,9 @@
+package com.scrumtrek.simplestore;
+
+import junit.framework.TestCase;
+
+public class MainClassTest extends TestCase {
+    public void testMain() throws Exception {
+        MainClass.Main(null);
+    }
+}
