c56e59ea02fde5efc8649cb9c847018fd13ecdfc
alexey-atiskov <atiskov@gmail.com>
2015-10-18 16:46:07 +0300

    add mockito

---
+++ src/main/test/com/scrumtrek/simplestore/RentalTest.java
@@ -0,0 +1,20 @@
+package com.scrumtrek.simplestore;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Alexey on 18.10.2015.
+ */
+public class RentalTest {
+    Mockito mockito;
+
+
+    @Test
+    public void testRental() {
+        assertTrue(true);
+    }
+
+}
