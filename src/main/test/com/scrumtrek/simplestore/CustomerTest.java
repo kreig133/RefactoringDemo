36fb6045fa30488771aeea68d8572ae9b1b39379
alexey-atiskov <atiskov@gmail.com>
2015-10-18 17:08:14 +0300

    change customer

--- src/main/test/com/scrumtrek/simplestore/CustomerTest.java
+++ src/main/test/com/scrumtrek/simplestore/CustomerTest.java
@@ -1,38 +1,41 @@
 package com.scrumtrek.simplestore;
 
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CustomerTest{
-    private String testString = "TEST";
+    private String CUSTOMER_NAME = "CUSTOMER_NAME";
+    private static final String TITLE = "title";
 
     @Test
     public void testGetName() {
-        final Customer customer = new Customer(testString);
-        assertEquals(customer.getName(), testString);
+        final Customer customer = new Customer(CUSTOMER_NAME);
+        assertEquals(customer.getName(), CUSTOMER_NAME);
     }
 
     @Test
     public void testNewRelease() {
-        final Customer customer = new Customer(testString);
-        final String title = "title";
-        final Movie movie = new Movie(title, PriceCodes.NewRelease);
-        final Rental rental = new Rental(movie, 10);
+        final Customer customer = new Customer(CUSTOMER_NAME);
+        final PriceCodes newRelease = PriceCodes.NewRelease;
+        final Rental rental = getRental(newRelease);
 
         customer.addRental(rental);
 
         final String statement = customer.Statement();
-        assertTrue(statement.contains(title + "\t"+ 30));
+        assertTrue(statement.contains(TITLE + "\t"+ 30));
 
-        assertTrue(customer.getName().equals(testString));
+        assertTrue(customer.getName().equals(CUSTOMER_NAME));
+    }
 
+    private Rental getRental(PriceCodes newRelease) {
+        final Movie movie = new Movie(TITLE, newRelease);
+        return new Rental(movie, 10);
     }
 
     @Test
-    public void testPriceCodes() throws Exception {
+    public void testRegular() throws Exception {
 
 
     }
