c8a092370e75c7fafd1404127b5d963552080dfc
kreig133 <kreig133@gmail.com>
2015-10-18 16:39:06 +0300

    change from groovy to java

---
+++ src/main/test/com/scrumtrek/simplestore/CustomerTest.java
@@ -0,0 +1,23 @@
+package com.scrumtrek.simplestore;
+
+
+import org.junit.Test;
+
+class CustomerTest{
+    private String testString = "TEST";
+
+
+    @Test
+    public void testGetName() {
+        new Customer(testString);
+    }
+
+    @Test
+    public void testAddRental() {
+
+    }
+    @Test
+    public void testStatement() {
+
+    }
+}
