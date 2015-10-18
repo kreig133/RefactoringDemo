88d0b6dd4e72c9ed3adeb7bf28fcb6e1576931f1
alexey-atiskov <atiskov@gmail.com>
2015-10-18 16:49:49 +0300

    add body of test

--- src/main/test/com/scrumtrek/simplestore/RentalTest.java
+++ src/main/test/com/scrumtrek/simplestore/RentalTest.java
@@ -3,18 +3,23 @@ package com.scrumtrek.simplestore;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Alexey on 18.10.2015.
  */
 public class RentalTest {
-    Mockito mockito;
 
+    private Movie movieMock = Mockito.mock(Movie.class);
 
     @Test
     public void testRental() {
-        assertTrue(true);
+        final int daysRented = 5;
+        final Rental rental = new Rental(movieMock, daysRented);
+
+        assertTrue(rental.getDaysRented() == daysRented);
+        assertNotNull(rental.getMovie());
     }
 
 }
