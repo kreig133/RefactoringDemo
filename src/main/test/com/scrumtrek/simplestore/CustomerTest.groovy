2dba2d3ae242a3827111dca07b01d81e87bc3bc8
kreig133 <kreig133@gmail.com>
2015-10-18 16:33:15 +0300

    New empty test

---
+++ src/main/test/com/scrumtrek/simplestore/CustomerTest.groovy
@@ -0,0 +1,19 @@
+package com.scrumtrek.simplestore
+
+
+class CustomerTest{
+    private String testString = "TEST";
+
+
+    void testGetName() {
+        new Customer(testSt);
+    }
+
+    void testAddRental() {
+
+    }
+
+    void testStatement() {
+
+    }
+}
