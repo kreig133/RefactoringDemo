dcad59d20d95c151162fa8cff552df8974ee18d3
kreig133 <kreig133@gmail.com>
2015-10-19 12:00:50 +0300

    remove MainClassTest

--- src/test/java/com/scrumtrek/simplestore/MainClassTest.java
+++
@@ -1,9 +0,0 @@
-package com.scrumtrek.simplestore;
-
-import junit.framework.TestCase;
-
-public class MainClassTest extends TestCase {
-    public void testMain() throws Exception {
-        MainClass.Main(null);
-    }
-}
